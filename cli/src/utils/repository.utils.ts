cfbf39829b967d50a1b2def9aac10861bf6d8034
Bertrand Marechal <bertrand.marechal@gadz.org>
2019-02-06 23:52:31 +0000

    playing with postgres analysis

--- cli/src/utils/repository.utils.ts
+++ cli/src/utils/repository.utils.ts
@@ -1,13 +1,33 @@
 import { FileUtils } from "./file.utils";
+import path from "path";
 
 export type RepositoryType = 'postgres' | 'serverless';
 
 export class RepositoryUtils {
-    static async readRepository (path: string, type: RepositoryType) {
+
+    private static processFileName(fileName: string) {
+        return fileName.replace(/[\/\/]|[\\\\]/g, '/');
+    }
+    static async readRepository (startPath: string, type: RepositoryType) {
         const files = await FileUtils.getFileList({
-            startPath: path,
+            startPath: startPath,
             filter: (type ==='postgres' ? /.sql/ : /serverless.yml/)
         });
-        console.log(files);
+
+        const newPath = RepositoryUtils.processFileName(startPath);
+        const result: any = {
+            path: newPath,
+            fileList: files
+                .map((x: string) => RepositoryUtils.processFileName(x).replace(newPath, ''))
+        };
+        for (let i = 0; i < files.length; i++) {
+            const element = files[i];
+            const file = await FileUtils.readFile(path.resolve(startPath, element));
+
+            const createTableRegex = /create\W+(or\W+replace\W+)?table\W+([a-z0-9_]+)\W/i;
+            console.log(file.match(createTableRegex));
+        }
+
+        console.log(JSON.stringify(result, null, 2));
     }
 }
