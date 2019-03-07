2598dc66d95a9f69f681f5ee687bc6313bf16624
Bertrand Marechal <bertrand.marechal@gadz.org>
2019-03-07 18:09:31 +0000

    cli bar

--- cli/src/classes/databse-file-helper.ts
+++ cli/src/classes/databse-file-helper.ts
@@ -3,6 +3,7 @@ import { FileUtils } from "../utils/file.utils";
 import { DatabaseRepositoryReader } from "./database-repo-reader";
 import path from 'path';
 import { LoggerUtils } from "../utils/logger.utils";
+import { Bar, Presets } from "cli-progress";
 
 const intentationSpaceNumber = 4;
 const indentationSpaces = ' '.repeat(intentationSpaceNumber);
@@ -14,7 +15,7 @@ export class DatabaseFileHelper {
         applicationName: string;
         filter: string;
     }): Promise<boolean> {
-        console.log('createFunctions');
+        LoggerUtils.info({origin: this._origin, message: `Getting ready to create functions.`});
 
         if (!params.applicationName) {
             throw 'Please provide an application name';
@@ -40,9 +41,17 @@ export class DatabaseFileHelper {
             'delete',
         ];
         let filesCreated = 0;
+        
+        const tables = Object.keys(databaseObject.table);
+        const bar = new Bar({
+            format: `Functions  [{bar}] {percentage}% | ETA: {eta}s | {value}/{total}`,
+            clearOnComplete: true
+        }, Presets.shades_grey);
+        bar.start(tables.length * 4, 0);
+
         // todo create the current folder
-        for (let t = 0; t < Object.keys(databaseObject.table).length; t++) {
-            const tableName = Object.keys(databaseObject.table)[t];
+        for (let t = 0; t < tables.length; t++) {
+            const tableName = tables[t];
             if (!databaseObject.table[tableName].tags.ignore) {
                 const dbParams: {[name: string]: string} = {
                     'entity_name': 'entity',
@@ -149,15 +158,44 @@ export class DatabaseFileHelper {
         
                         for (let j = 0; j < dbParamsFields.length; j++) {
                             const param = dbParamsFields[j];
-                            fileString = fileString.replace(new RegExp(`<${param}>`, 'gi'), dbParams[param]);
+                            let valueToReplaceWith = dbParams[param];
+
+                            switch (param) {
+                                case 'roles':
+                                    let roles = '';
+                                    if (databaseObject.table[tableName].tags[`${action}-roles`]) {
+                                        if (databaseObject.table[tableName].tags[`${action}-roles`].value.match(/^\[[a-zA-Z0-9-_' ,]+\]$/)) {
+                                            roles = databaseObject.table[tableName].tags[`${action}-roles`].value;
+                                        }
+                                    }
+                                    if (databaseObject.table[tableName].tags[`roles`]) {
+                                        if (databaseObject.table[tableName].tags[`roles`].value.match(/^\[[a-zA-Z0-9-_' ,]+\]$/)) {
+                                            roles = databaseObject.table[tableName].tags[`roles`].value;
+                                        }
+                                    }
+                                    roles = roles
+                                        .replace(/\'/g, '"')
+                                        .replace(/\[/g, '{')
+                                        .replace(/\]/g, '}');
+                                    if (roles) {
+                                        valueToReplaceWith = `'${roles}'`;
+                                    }
+                                    break;
+                                default:
+                                    
+                                    break;
+                            }
+                            fileString = fileString.replace(new RegExp(`<${param}>`, 'gi'), valueToReplaceWith);
                         }
         
                         FileUtils.writeFileSync(path.resolve(folderPath, `${dbParams.db_prefix}f_${action}_${nameWithoutPrefixAndSuffix}.sql`), fileString);
                         filesCreated++;
+                        bar.update(filesCreated + 1);
                     }
                 }
             }
         }
+        bar.stop();
         if (filesCreated) {
             LoggerUtils.success({origin: this._origin, message: `Created ${filesCreated} files.`});
         } else {
